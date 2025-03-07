90fa714436af50fe6e46468112f3e2bc36b70163
Berezovskii Semyon <ber2semen@gmail.com>
2025-03-07 12:21:44 +0700

    переработан ci/cd, пытаюсь запустить в браузере

--- okr_backend/Program.cs
+++ okr_backend/Program.cs
@@ -52,13 +52,13 @@ builder.Services.AddAuthentication(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 
 //---
